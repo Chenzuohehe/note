857daa004e342c4d2c8ad92cf3c751873259b852
陈佐 <chenzuo0730@outlook.com>
2017-07-24 15:51:12 +0800

    commit

---
+++ note/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  note
+//
+//  Created by ChenZuo on 2017/7/24.
+//  Copyright © 2017年 ChenZuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
